402590ede4337f5e8f19dd6961d7c7acf35f674d
radtradcath <vinicius.b.bonafini@gmail.com>
2023-08-01 13:53:48 -0300

    Fix operator button bug
    -Previous commit caused operator button to crash calculator.
    Altered conditional for void(0) return to permit sequencial operations
    before pressing equal;

--- javascript.js
+++ javascript.js
@@ -1,3 +1,96 @@
+const btns = document.querySelectorAll('button');
+const numBtns = document.querySelectorAll('.number');
+const oprBtns = document.querySelectorAll('.operator');
+const display = document.querySelector('#calc-display');
+const calcArray = document.querySelector('#calc-array');
+let currentDisplay;
+let inputArr = [];
+let clicked = false;
+
+btns.forEach(btn => {
+    
+if (btn.getAttribute('class') === 'number') {
+        btn.addEventListener('click', () => {            
+            if (display.textContent == 'Math Error' || display.textContent.length > 15 || clicked == true) {
+                return void(0);
+            } else if (btn.getAttribute('id') === 'dot' && display.textContent.includes('.')) {
+                return void(0);
+            }
+            display.textContent += btn.textContent;
+            currentDisplay = display.textContent;
+
+        });
+    } else if (btn.getAttribute('class') === 'operator') {
+        btn.addEventListener('click', () => {                                               
+            if (((inputArr[inputArr.length-1] === '+' || inputArr[inputArr.length-1] == '-' || inputArr[inputArr.length-1] == '*' || inputArr[inputArr.length-1] == '/') && display.textContent == '') || display.textContent == '' || display.textContent == 'Math Error') {
+                return void(0);
+            }
+            clicked = false;
+            inputArr.push(display.textContent);
+            switch (btn.textContent) {
+                case '+':
+                    inputArr.push('+');
+                    break;
+                case '-':
+                    inputArr.push('-');
+                    break;
+                case '*':
+                    inputArr.push('*');
+                    break;
+                case '/':
+                    inputArr.push('/');
+                    break;
+                default:                    
+            }
+            display.textContent = ''; 
+            calcArray.textContent = inputArr.join("");
+        });
+    } else if (btn.getAttribute('id') === 'eq') {
+        btn.addEventListener('click', () => {
+            if (display.textContent == "" || display.textContent == 'Math Error' || inputArr.length < 2) {
+                return void(0);
+            }
+            inputArr.push(currentDisplay);                     
+            calcArray.textContent = `${inputArr.join("")} =`;
+            do {
+            operate(inputArr[0], inputArr[1], inputArr[2]);            
+            inputArr.shift();
+            inputArr.shift(); 
+            inputArr.shift();         
+            inputArr.unshift(result);
+            } while (inputArr.length > 1);
+            if (result == 'Math Error') {                     
+            display.textContent = result;
+            } else {
+            display.textContent = +result.toFixed(8);
+            
+            }
+            currentDisplay = display.textContent;
+            inputArr = [];     
+            clicked = true;
+        });
+    } else if (btn.getAttribute('id') === 'clear') {
+        btn.addEventListener('click', () => {
+            inputArr = [];
+            display.textContent = '';
+            calcArray.textContent = inputArr.join("");
+            clicked = false;
+            
+        });
+    } else if (btn.getAttribute('id') === 'del') {
+        btn.addEventListener('click', () => {
+            if (display.textContent == 'Math Error' || clicked == true){
+                return void(0);
+            }
+            let displayArr = display.textContent.split("");
+            displayArr.pop();
+            display.textContent = displayArr.join("");
+            calcArray.textContent = inputArr.join("");
+            
+        }) 
+    }; 
+});
+
 let num_a;
 let num_b;
 let opr;
@@ -49,97 +142,3 @@ let operate = function (a, opr, b) {
             return "Input a valid operation";
     }
 }
-
-const btns = document.querySelectorAll('button');
-const numBtns = document.querySelectorAll('.number');
-const oprBtns = document.querySelectorAll('.operator');
-const display = document.querySelector('#calc-display');
-const calcArray = document.querySelector('#calc-array');
-let currentDisplay;
-let inputArr = [];
-let clicked = false;
-
-btns.forEach(btn => {
-    
-if (btn.getAttribute('class') === 'number') {
-        btn.addEventListener('click', () => {            
-            if (display.textContent == 'Math Error' || display.textContent.length > 15 || clicked == true) {
-                return void(0);
-            } else if (btn.getAttribute('id') === 'dot' && display.textContent.includes('.')) {
-                return void(0);
-            }
-            display.textContent += btn.textContent;
-            currentDisplay = display.textContent;
-
-        });
-    } else if (btn.getAttribute('class') === 'operator') {
-        btn.addEventListener('click', () => {                        
-            if (inputArr[inputArr.length-1] == '+' || inputArr[inputArr.length-1] == '-' || inputArr[inputArr.length-1] == '*' || inputArr[inputArr.length-1] == '/' || display.textContent == '' || display.textContent == 'Math Error') {
-                return void(0);
-            }
-            clicked = false;
-            inputArr.push(currentDisplay);
-            display.textContent = '';            
-            switch (btn.textContent) {
-                case '+':
-                    inputArr.push('+');
-                    break;
-                case '-':
-                    inputArr.push('-');
-                    break;
-                case '*':
-                    inputArr.push('*');
-                    break;
-                case '/':
-                    inputArr.push('/');
-                    break;
-                default:                    
-            }
-            calcArray.textContent = inputArr.join("");
-        });
-    } else if (btn.getAttribute('id') === 'eq') {
-        btn.addEventListener('click', () => {
-            if (display.textContent == "" || display.textContent == 'Math Error' || inputArr.length < 2) {
-                return void(0);
-            }
-            inputArr.push(currentDisplay);                     
-            calcArray.textContent = inputArr.join("");
-            do {
-            operate(inputArr[0], inputArr[1], inputArr[2]);            
-            inputArr.shift();
-            inputArr.shift(); 
-            inputArr.shift();         
-            inputArr.unshift(result);
-            } while (inputArr.length > 1);
-            if (result == 'Math Error') {                     
-            display.textContent = result;
-            } else {
-            display.textContent = +result.toFixed(8);
-            }
-            currentDisplay = display.textContent;
-            inputArr = [];     
-            clicked = true;
-        });
-    } else if (btn.getAttribute('id') === 'clear') {
-        btn.addEventListener('click', () => {
-            inputArr = [];
-            display.textContent = '';
-            calcArray.textContent = inputArr.join("");
-            clicked = false;
-            
-        });
-    } else if (btn.getAttribute('id') === 'del') {
-        btn.addEventListener('click', () => {
-            if (display.textContent == 'Math Error' || clicked == true){
-                return void(0);
-            }
-            let displayArr = display.textContent.split("");
-            displayArr.pop();
-            display.textContent = displayArr.join("");
-            calcArray.textContent = inputArr.join("");
-            
-        }) 
-    }; 
-});
-
-
